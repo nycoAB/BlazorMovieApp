b1d121ed2552bb8b7542cb8231e04e881bec5ac6
AbolFazl <abolfazl.akhondi96@gmail.com>
2024-11-17 22:06:43 +0330

    initial commit

---
+++ Data/MovieAppContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+
+namespace MovieApp.Data
+{
+    public class MovieAppContext : DbContext
+    {
+        public MovieAppContext (DbContextOptions<MovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Movie>().HasData
+                (
+                new Movie
+                {
+                    Id = 1,
+                    Title = "Mad Max",
+                    Genre = "Sci-fi (Cyberpunk)",
+                    ReleaseDate = new DateOnly(1979, 4, 12) ,
+                    Price = 2.51M,
+                },
+                
+                new Movie
+                {
+                    Id= 2,
+                    Title = "Mad Max: Fury Road",
+                    ReleaseDate = new DateOnly(2015, 5, 15),
+                    Genre = "Sci-fi (Cyberpunk)",
+                    Price = 8.43M
+                });
+        }
+    }
+}
---
+++ Migrations/20241115164020_SeedData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MovieApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movie",
+                columns: new[] { "Id", "Genre", "Price", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Sci-fi (Cyberpunk)", 2.51m, new DateOnly(1979, 4, 12), "Mad Max" },
+                    { 2, "Sci-fi (Cyberpunk)", 8.43m, new DateOnly(2015, 5, 15), "Mad Max: Fury Road" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
